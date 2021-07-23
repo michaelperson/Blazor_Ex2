65a18ffe2c2999d2104d1b2b71da1251f0a50c53
Person M <michael.person@cognitic.be>
2021-07-23 13:51:15 +0200

    Initial Commit

---
+++ Ex2/Client/Components/Form/SampleForm.razor.cs
@@ -0,0 +1,64 @@
+﻿using Ex2.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex2.Client.Components.Form
+{
+    public class SampleFormBase: ComponentBase
+    {
+        private Contact _contact;
+        protected string Error = string.Empty;
+        protected bool Success = false;
+        public Contact Contact
+        {
+            get
+            {
+                return _contact;
+            }
+
+            set
+            {
+                _contact = value;
+            }
+        }
+
+        public SampleFormBase()
+        {
+            Contact = new Contact();
+            Contact.Couleur = Color.White;
+        }
+
+        public void ValideForm()
+        {
+            //afficher le message à la plce du formulaire
+            Success = true;
+            Error = string.Empty;
+        }
+
+        public void InvalideForm()
+        {
+            //afficher le message en rouge
+            Error = "Veuillez vérifier le formulaire";
+        }
+
+        //public void SubmitForm(EditContext ctx)
+        //{
+        //    if(ctx.Validate())
+        //    {
+        //        //afficher le message à la plce du formulaire
+        //        Success = true;
+        //        Error = string.Empty;
+        //    }
+        //    else
+        //    {
+        //        //afficher le message en rouge
+        //        Error = "Veuillez vérifier le formulaire";
+        //    }
+        //}
+    }
+}
---
+++ Ex2/Shared/Contact.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2.Shared
+{
+    public class Contact
+    {
+        private string _nom, _email, _telephone, _message;
+        private Color _couleur;
+
+        [Required]
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+        [EmailAddress]
+        [Required]
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                _email = value;
+            }
+        }
+        [Required] 
+        public string Telephone
+        {
+            get
+            {
+                return _telephone;
+            }
+
+            set
+            {
+                _telephone = value;
+            }
+        }
+        [Required]
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+
+            set
+            {
+                _message = value;
+            }
+        }
+        [Required]
+        public Color Couleur
+        {
+            get
+            {
+                return _couleur;
+            }
+
+            set
+            {
+                _couleur = value;
+            }
+        }
+    }
+}
